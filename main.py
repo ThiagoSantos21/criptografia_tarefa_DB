bb809144d626995bc69735498d254c6c232993ba
Thiago Lima Santos <81772083+ThiagoSantos21@users.noreply.github.com>
2023-05-26 10:07:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ import json
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://thiago:1234qwer@cluster0.ez0960m.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
