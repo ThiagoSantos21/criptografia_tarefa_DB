401be4ccde3e3f468bfb348485db2aebf345c3ef
Elias Fausto <elias-fausto@hotmail.com>
2023-06-01 21:04:31 -0300

    Adicionando no BD

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from pymongo.server_api import ServerApi
 
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient("mongodb+srv://thiago:1234qwer@cluster0.ez0960m.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
 
 # Send a ping to confirm a successful connection
 try:
@@ -48,41 +48,19 @@ def enviar_mensagem():
     mensagem_chave = input()
     chave = gerar_chave_fernet(mensagem_chave.encode('utf-8'))
     fernet = Fernet(chave)
+
     return fernet.encrypt(mensagem.encode('utf-8'))
+       
     # print(f"string para gerar a chave: {mensagem_chave}")
     # print(f"chave gerada: {chave}")
     # print(f"string para gerar a mensagem: {mensagem}")
-    # print(f"mensagem criptografada: {texto_cifrado}")    
-
-def menu():
-    print('Login: ', end='')
-    user = input()
-    print(f'Olá {user}, o que deseja fazer ?')
-    print('A) Enviar uma mensagem')
-    print('B) Ler mensagens do banco')
-    op = input()
-    op = str.upper(op)
-    match op:
-        case 'A':
-            print('sexo')
-            mensagem = enviar_mensagem()
-        case 'B' :
-            print('oxes')
-        case _ :
-            print('Opção inválida - Refaça a operação')
-            menu()
-            
-
-menu()
-
-# Gerar arquivo json
 
 def gerar_json(user, mensagem):
     dados = {
         "from": user,
         "to": "Thiago",
         "wasRead": False,
-        "message": mensagem
+        "message": mensagem.decode('utf-8')
     }
 
     # Caminho e nome do arquivo JSON
@@ -94,6 +72,68 @@ def gerar_json(user, mensagem):
         json.dump(dados, arquivo)
 
     print("Arquivo JSON gerado com sucesso!")
+
+def enviar_para_mongodb(nome_arquivo):
+    # Selecionar o banco de dados
+    db = client['ChatEncrypt']
+
+    # Selecionar a coleção
+    collection = db['Chat']
+
+    # Ler o arquivo JSON
+    with open(nome_arquivo, 'r') as arquivo:
+        dados = json.load(arquivo)
+
+    # Inserir os dados na coleção
+    collection.insert_one(dados)
+
+    print("Arquivo JSON enviado para o MongoDB com sucesso!")
+
+def ler_mensagens(chave):
+    # Selecionar o banco de dados
+    db = client['ChatEncrypt']
+
+    # Selecionar a coleção
+    collection = db['Chat']
+
+    # Consultar as mensagens na coleção
+    mensagens = collection.find()
+
+    fernet = Fernet(chave)
+
+    # Exibir as mensagens
+    for mensagemBD in mensagens:
+        print(f"Mensagem de: {mensagemBD['from']}")
+        mensagem_cifrada = mensagemBD['message'].encode('utf-8')
+        mensagem_decifrada = fernet.decrypt(mensagem_cifrada).decode('utf-8')
+        print(f"Mensagem: {mensagem_decifrada}")
+        print()
+
+
+def menu():
+    print('Login: ', end='')
+    user = input()
+    print(f'Olá {user}, o que deseja fazer ?')
+    print('A) Enviar uma mensagem')
+    print('B) Ler mensagens do banco')
+    op = input()
+    op = str.upper(op)
+    match op:
+        case 'A':
+            mensagem = enviar_mensagem()
+            gerar_json(user, mensagem)
+            enviar_para_mongodb("message.json")
+        case 'B' :
+            print('Digite a chave para decifrar a mensagem: ', end='')
+            chave = input().encode('utf-8')
+            ler_mensagens(chave)
+        case _ :
+            print('Opção inválida - Refaça a operação')
+            menu()
+            
+menu()
+
+# Gerar arquivo json
 
     
 # texto_chave_secreta = "Vasco da Gama"
